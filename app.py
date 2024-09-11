8fb2b15d9d03c384766d1bd30b14a0476a377155
TYT14 <tstang@ca-cf.fr>
2024-09-11 15:47:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,16 @@ st.slider("Sélectionner un âge", min_value=20, max_value=100, value=30, step=1
 if st.checkbox("Affichage le jeu de données"):
   st.write(df[(df.Profession == user_selection)])
 
+# 1 Bouton upload .csv
+
+st.checkbox("Uploader un fichier svp")
+
+# 2 Selection d'un variable
+
+# 3 Affichage d'un graphique
+
+# 4 
+
+
+
 
