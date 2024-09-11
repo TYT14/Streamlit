78c0600e897fadefbe18b3ee2aa62b49c2dd8804
TYT14 <tstang@ca-cf.fr>
2024-09-11 15:00:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ user_selection = st.selectbox('Selexctionner une profession', pro)
 st.slider("Sélectionner un âge", min_value=20, max_value=100, value=30, step=1)
 
 if st.checkbox("Affichage le jeu de données"):
-  st.write(df[df.Profession == user_selection])
+  st.write(df[(df.Profession == user_selection) & (df.Age ==age)])
 
 
