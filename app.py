dd29b5dc388992fb0f65f1536d491b2efbd7a1e7
TYT14 <tstang@ca-cf.fr>
2024-09-11 16:19:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,7 @@ import requests
 
 st.title("My Dashboard")
 
-df = pd.read_csv('data.csv')
 
-if st.checkbox("affichage le jeu de données"):
-  st.write(df)
-
-pro = df.Profession.unique()
-
-user_selection = st.selectbox('Selexctionner une profession', pro)
-
-st.slider("Sélectionner un âge", min_value=20, max_value=100, value=30, step=1)
-
-if st.checkbox("Affichage le jeu de données"):
-  st.write(df[(df.Profession == user_selection)])
 
 # 1 Bouton upload .csv
 
