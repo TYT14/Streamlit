008cb9fc97920cc8cdfa64ed3e2ea0b8d947c898
TYT14 <tstang@ca-cf.fr>
2024-09-11 16:52:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,15 +19,15 @@ if st.checkbox("Uploader un fichier svp") :
     dataframe = pd.read_csv(uploaded_file,sep=';')
     st.write(dataframe)
 
-# 2 selection d'une variable avec df.columns & st.selectbox
-columns = dataframe.columns
-user_selection = st.selectbox('Selectionner une variable', columns)
-
-# 3 Affichage de la varable sous forme de graphique
-columns_selected = dataframe[user_selection]
-fig, ax = plt.subplots()
-ax.hist(columns_selected, bins=20)
-
-st.pyplots(fig)
+    # 2 selection d'une variable avec df.columns & st.selectbox
+    columns = dataframe.columns
+    user_selection = st.selectbox('Selectionner une variable', columns)
+    
+    # 3 Affichage de la varable sous forme de graphique
+    columns_selected = dataframe[user_selection]
+    fig, ax = plt.subplots()
+    ax.hist(columns_selected, bins=20)
+    
+    st.pyplots(fig)
 
 
