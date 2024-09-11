95e8a41653da06ec24e39744707dc7aaac98a5ea
TYT14 <tstang@ca-cf.fr>
2024-09-11 16:33:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import requests
+from io imports StringIO
+import seaborn as sns
 
 
 st.title("My Dashboard")
@@ -12,14 +14,14 @@ st.title("My Dashboard")
 if st.checkbox("Uploader un fichier svp") :
  uploaded_file = st.file_uploader("Choisir un fichier pour upload")
  if uploaded_file is not None:
-    # To read file as bytes:
-    
-
-    
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file,sep=';')
+    dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
 
 # 2 selection d'une variable avec df.columns & st.selectbox
-user_selection = st.selectbox('Selectionner une variable', df.columns)
+columns = dataframe.columns
+user_selection = st.selectbox('Selectionner une variable', columns)
+
+# 3 Affichage de la varable sous forme de graphique
+st.write(dataframe)
+
 
