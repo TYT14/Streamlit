1b0878a881f704ae54a7d1618ecef28bf8be4331
TYT14 <tstang@ca-cf.fr>
2024-09-11 16:41:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.title("My Dashboard")
 if st.checkbox("Uploader un fichier svp") :
  uploaded_file = st.file_uploader("Choisir un fichier pour upload")
  if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
+    dataframe = pd.read_csv(uploaded_file,sep=';')
     st.write(dataframe)
 
 # 2 selection d'une variable avec df.columns & st.selectbox
