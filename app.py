161cff6b5650d47fb171d3c896afb5e038172008
TYT14 <tstang@ca-cf.fr>
2024-09-11 14:56:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ if st.checkbox("affichage le jeu de données"):
 
 pro = df.Profession.unique()
 
-st.selectbox('Selexctionner une profession', pro)
+user_selection = st.selectbox('Selexctionner une profession', pro)
 
-st.write(df)
+st.slider("Sélectionner un âge", min_value=20, max_value=100, value=30, step=1)
+
+if st.checkbox("Affichage le jeu de données"):
+  st.write(df[df.profession == user_selection])
+
+
