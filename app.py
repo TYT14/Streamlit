aec771eb2f81ad9f8a6c58e0efdc97767869dd32
TYT14 <tstang@ca-cf.fr>
2024-09-11 16:08:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,14 +28,7 @@ if st.checkbox("Uploader un fichier svp") :
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
+    
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file,sep=';')
     st.write(dataframe)
